30f3bc32b1ecdd44bd48bece56c4a05a24f56a1d
Nick Christiny <email": null>
2015-10-26 18:32:39 -0500

    Implement Rails migrations and models

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+  # copied/pasta from BMock/NChristiny sinatra-to-rails-pick-1-of-3-challenge/hacker-news
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username,:password)
+    end
+
+end
