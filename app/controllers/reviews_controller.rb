3b0138e22ee2256ab5d3ee2fdcc80d80c7611b71
Andy Dierker & Linas Mikalcius <pair+adierker.linas1985@devbootcamp.com>
2015-10-27 14:00:10 -0500

    add some features

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,27 @@
 class ReviewsController < ApplicationController
+
+  def new
+    @game = Game.find(params[:game_id])
+    @review = @game.reviews.new
+    p params
+  end
+
+  def create
+    @review = Game.find(params[:game_id]).reviews.new(review_params)
+    @reviewer = current_user
+    if @review.save
+      redirect_to game_path(Game.find(params[:game_id]))
+    else
+      render 'new'
+    end
+
+  end
+
+
+  private
+
+  def review_params
+    params.require(:review).permit(:score, :title, :body)
+  end
+
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -5,4 +5,13 @@ class Game < ActiveRecord::Base
   has_many :reviewers, through: :reviews, source: :reviewer
   has_many :ownerships
   has_many :owners, through: :ownerships, source: :owner
+
+  def average
+    if self.reviews.length > 0
+      self.reviews.map(&:score).reduce(:+)/self.reviews.length
+    else
+      return "no reviews yet"
+    end
+  end
+
 end
