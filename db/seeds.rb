8558634f203be9a0d47a146dea257c53080753fe
Nick Christiny <email": null>
2015-10-27 09:35:24 -0500

    working on homepage

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,8 +16,8 @@
   categorization = Category.all.sample.categorizations.create!(game: Game.all.sample)
 
   review = Game.all.sample.reviews.create!(reviewer: User.all.sample,
-                                          title: Faker::Lorem.sentences(1),
-                                          body: Faker::Lorem.paragraph,
+                                          title: Faker::Hacker.say_something_smart,
+                                          body: Faker::Hacker.say_something_smart,
                                           score: Faker::Number.between(1, 5),
                                           created_at: Faker::Time.between(500.days.ago, Time.now, :all))
 
@@ -34,10 +34,45 @@ end
 
 # real games go here
 
-  game = Category.all.sample.games.create!(name: Faker::App.name,
-                      image_url: Faker::Company.logo,
-                      description: Faker::Lorem.paragraph,
-                      min_players:  Faker::Number.between(1, 2),
-                      max_players:  Faker::Number.between(3, 10),
-                      created_at: Faker::Time.between(500.days.ago, Time.now, :all))
+game = Category.all.sample.games.create!(name: "Checkers",
+                    image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/International_draughts.jpg/240px-International_draughts.jpg",
+                    description: "A game about jumping and kings",
+                    min_players:  1,
+                    max_players:  2,
+                    created_at: Faker::Time.between(500.days.ago, Time.now, :all))
+
+game = Category.all.sample.games.create!(name: Faker::App.name,
+                image_url: Faker::Company.logo,
+                description: Faker::Lorem.paragraph,
+                min_players:  Faker::Number.between(1, 2),
+                max_players:  Faker::Number.between(3, 10),
+                created_at: Faker::Time.between(500.days.ago, Time.now, :all))
+
+game = Category.all.sample.games.create!(name: Faker::App.name,
+              image_url: Faker::Company.logo,
+              description: Faker::Lorem.paragraph,
+              min_players:  Faker::Number.between(1, 2),
+              max_players:  Faker::Number.between(3, 10),
+              created_at: Faker::Time.between(500.days.ago, Time.now, :all))
+
+game = Category.all.sample.games.create!(name: Faker::App.name,
+              image_url: Faker::Company.logo,
+              description: Faker::Lorem.paragraph,
+              min_players:  Faker::Number.between(1, 2),
+              max_players:  Faker::Number.between(3, 10),
+              created_at: Faker::Time.between(500.days.ago, Time.now, :all))
+
+game = Category.all.sample.games.create!(name: Faker::App.name,
+              image_url: Faker::Company.logo,
+              description: Faker::Lorem.paragraph,
+              min_players:  Faker::Number.between(1, 2),
+              max_players:  Faker::Number.between(3, 10),
+              created_at: Faker::Time.between(500.days.ago, Time.now, :all))
+
+game = Category.all.sample.games.create!(name: Faker::App.name,
+              image_url: Faker::Company.logo,
+              description: Faker::Lorem.paragraph,
+              min_players:  Faker::Number.between(1, 2),
+              max_players:  Faker::Number.between(3, 10),
+              created_at: Faker::Time.between(500.days.ago, Time.now, :all))
 
