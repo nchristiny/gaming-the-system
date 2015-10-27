14520c32d533749b538ad0b6b1c06e11d8dd1d1b
Nick Christiny <email": null>
2015-10-27 09:27:59 -0500

    working on homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
+
   def index
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+      @friends_recent_ownerships = @user.friends.map(&:ownerships).sort {|a,b| a.created_at <=> b.created_at}.last(5)
+      @friends_recent_reviews = @user.friends.map(&:reviews).sort {|a,b| a.created_at <=> b.created_at}.last(5)
+    end
   end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 25.times do
 
   user = User.create!(username: Faker::Internet.user_name,
-                      password_digest: Faker::Internet.password)
+                      password_digest: Faker::Internet.password,
+                      created_at: Faker::Time.between(500.days.ago, Time.now, :all))
 
   category = Category.create!(name: Faker::Company.buzzword)
 
@@ -9,18 +10,34 @@
                       image_url: Faker::Company.logo,
                       description: Faker::Lorem.paragraph,
                       min_players:  Faker::Number.between(1, 2),
-                      max_players:  Faker::Number.between(3, 10))
+                      max_players:  Faker::Number.between(3, 10),
+                      created_at: Faker::Time.between(500.days.ago, Time.now, :all))
 
   categorization = Category.all.sample.categorizations.create!(game: Game.all.sample)
 
   review = Game.all.sample.reviews.create!(reviewer: User.all.sample,
                                           title: Faker::Lorem.sentences(1),
                                           body: Faker::Lorem.paragraph,
-                                          score: Faker::Number.between(1, 5))
+                                          score: Faker::Number.between(1, 5),
+                                          created_at: Faker::Time.between(500.days.ago, Time.now, :all))
 
 end
 
 50.times do
-  friendship = User.all.sample.friendships.create!(friend: User.all.sample)
-  ownership = User.all.sample.ownerships.create!(game: Game.all.sample)
+  friendship = User.all.sample.friendships.create!(friend: User.all.sample,
+                                                  created_at: Faker::Time.between(500.days.ago, Time.now, :all))
+  ownership = User.all.sample.ownerships.create!(game: Game.all.sample,
+                                                created_at: Faker::Time.between(500.days.ago, Time.now, :all))
 end
+
+
+
+# real games go here
+
+  game = Category.all.sample.games.create!(name: Faker::App.name,
+                      image_url: Faker::Company.logo,
+                      description: Faker::Lorem.paragraph,
+                      min_players:  Faker::Number.between(1, 2),
+                      max_players:  Faker::Number.between(3, 10),
+                      created_at: Faker::Time.between(500.days.ago, Time.now, :all))
+
