a290fd39877ba8ff40c917abea2141304ceb5245
Andy Dierker <dierker@gmail.com>
2015-10-27 01:29:13 -0500

    seeded, signup works, login does not work

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+25.times do
+
+  user = User.create!(username: Faker::Internet.user_name,
+                      password_digest: Faker::Internet.password)
+
+  category = Category.create!(name: Faker::Company.buzzword)
+
+  game = Category.all.sample.games.create!(name: Faker::App.name,
+                      image_url: Faker::Company.logo,
+                      description: Faker::Lorem.paragraph,
+                      min_players:  Faker::Number.between(1, 2),
+                      max_players:  Faker::Number.between(3, 10))
+
+  categorization = Category.all.sample.categorizations.create!(game: Game.all.sample)
+
+  review = Game.all.sample.reviews.create!(reviewer: User.all.sample,
+                                          title: Faker::Lorem.sentences(1),
+                                          body: Faker::Lorem.paragraph,
+                                          score: Faker::Number.between(1, 5))
+
+end
+
+50.times do
+  friendship = User.all.sample.friendships.create!(friend: User.all.sample)
+  ownership = User.all.sample.ownerships.create!(game: Game.all.sample)
+end
